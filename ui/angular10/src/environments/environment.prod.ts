77c274dbf3b137445c0d3b19a5e1906e492a2062
Mathis <mathisgarcia14@gmail.com>
2021-07-02 16:17:38 +0200

    Change apiUrl to localhost for prod

--- ui/angular10/src/environments/environment.prod.ts
+++ ui/angular10/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   staticURL: 'static/angular/',
-  apiUrl: "http://solutions.diametrix.fr:8082",
+  apiUrl: "http://localhost:8000",
   apiBanUrl: "https://api-adresse.data.gouv.fr"
 };
