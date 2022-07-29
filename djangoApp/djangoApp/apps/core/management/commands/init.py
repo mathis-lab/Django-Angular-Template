3bddb36652fbb6683f088642caea5f2cddbca275
mathis <mathisgarcia14@gmail.com>
2022-07-29 23:29:47 +0200

    Fix database init

--- djangoApp/djangoApp/apps/core/management/commands/init.py
+++ djangoApp/djangoApp/apps/core/management/commands/init.py
@@ -5,5 +5,8 @@ from django.apps import apps
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Init database..."))
-        apps.get_model("core", "User").objects.init_table()
-        self.stdout.write(self.style.SUCCESS("Database init success !"))
+        if apps.get_model("core", "User").objects.count() == 0:
+            apps.get_model("core", "User").objects.init_table()
+            self.stdout.write(self.style.SUCCESS("Database init success !"))
+        else:
+            self.stdout.write(self.style.SUCCESS("Database already init !"))
